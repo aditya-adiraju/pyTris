f883d2afd119d3786d8f97d2623164c180bf7f47
aditya-adiraju <adadiraju@cisb.org.in>
2021-04-11 16:01:59 +0530

    Added colours and fixed rotation
    >Added individual colours to each tetromino.
    
    >Added a new background to the game as well reduced the game window size
    
    >Fixed major rotation bugs present in the previous version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 screen_x = 250
 screen_y = 600
 pygame.init()
-screen = pygame.display.set_mode((screen_x + 150, screen_y))
+screen = pygame.display.set_mode((screen_x, screen_y))
 pygame.display.set_caption("PyTris")
 icon = pygame.image.load("Assets/icon.png")
 pygame.display.set_icon(icon)
@@ -13,24 +13,24 @@ clock = pygame.time.Clock()
 
 class Player:
     def __init__(self):
-        self.grid = [[0 for x in range(10)] for y in range(24)]
+        self.grid = [[(0, 0, 0) for x in range(10)] for y in range(24)]
 
     def check_clear(self):
         flag = True
         for y in range(len(self.grid)):
-            if self.grid[y].count(0) == 0:
+            if self.grid[y].count((0, 0, 0)) == 0:
                 flag = False
                 for i in range(y-1, -1, -1):
                     self.grid[i+1] = self.grid[i]
-                self.grid[0] = [0 for x in range(10)]
+                self.grid[0] = [(0, 0, 0) for x in range(10)]
         if not flag:
             self.check_clear()
 
     def draw(self, win):
         for y in range(len(self.grid)):
             for x in range(len(self.grid[y])):
-                if self.grid[y][x] == 1:
-                    pygame.draw.rect(win, (255, 0, 0), ((x * 25, y * 25), (25, 25)))
+                if self.grid[y][x] != (0, 0, 0):
+                    pygame.draw.rect(win, self.grid[y][x], (((x * 25) + 2, (y * 25) + 2), (23, 23)))
 
 
 new = Player()
@@ -46,92 +46,117 @@ class Tetromino:
             self.location = [[self.y, self.x], [self.y + 1, self.x], [self.y + 2, self.x], [self.y + 3, self.x]]
             self.width = 1
             self.height = 4
+            self.center = [self.y + 2, self.x + 0.5]
+            self.color = (173, 216, 255)
         elif self.s == 'L':
             self.location = [[self.y, self.x], [self.y + 1, self.x], [self.y + 2, self.x], [self.y + 2, self.x + 1]]
             self.width = 2
             self.height = 3
+            self.center = [self.y + 1.5, self.x + 1]
+            self.color = (255, 165, 0)
         elif self.s == 'J':
             self.location = [[self.y, self.x + 1], [self.y + 1, self.x + 1], [self.y + 2, self.x + 1],
                              [self.y + 2, self.x]]
             self.width = 2
             self.height = 3
+            self.center = [self.y + 1.5, self.x + 1]
+            self.color = (0, 0, 255)
         elif self.s == 'S':
             self.location = [[self.y, self.x], [self.y + 1, self.x], [self.y + 1, self.x + 1], [self.y + 2, self.x + 1]]
             self.width = 2
             self.height = 3
+            self.center = [self.y + 1.5, self.x + 1]
+            self.color = (0, 255, 100)
         elif self.s == 'Z':
             self.location = [[self.y, self.x + 1], [self.y + 1, self.x + 1], [self.y + 1, self.x], [self.y + 2, self.x]]
             self.width = 2
             self.height = 3
+            self.center = [self.y + 1.5, self.x + 1]
+            self.color = (255, 0, 0)
         elif self.s == 'T':
             self.location = [[self.y, self.x], [self.y + 1, self.x], [self.y + 1, self.x + 1], [self.y + 2, self.x]]
             self.width = 2
             self.height = 3
+            self.center = [self.y + 1.5, self.x + 1]
+            self.color = (175, 53, 139)
         elif self.s == 'O':
             self.location = [[self.y, self.x], [self.y, self.x + 1], [self.y + 1, self.x], [self.y + 1, self.x + 1]]
             self.width = 2
             self.height = 2
-        self.fill(new)
+            self.center = [self.y + 1, self.x + 1]
+            self.color = (255, 255, 0)
+        self.fill(new, self.color)
 
-    def fill(self, player=new, f=1):
+    def fill(self, player=new, f=(0, 0, 0)):
         for i in self.location:
             player.grid[i[0]][i[1]] = f
 
+    def reset_center(self):
+        min_x = self.location[0][1]
+        max_x = self.location[0][1]
+        for i in self.location:
+            if i[1] > max_x:
+                max_x = i[1]
+            elif i[1] < min_x:
+                min_x = i[1]
+        self.center[1] = (min_x + max_x + 1) / 2
+        self.center[0] = (min(self.location)[0] + max(self.location)[0] + 1) / 2
+
     def collide(self, player=new):
         for i in self.location:
-            if player.grid[i[0]][i[1]] == 1:
+            if player.grid[i[0]][i[1]] != (0, 0, 0):
+
                 return True
         return False
 
     def rotate(self):
-        self.fill(new, 0)
-        for mino in self.location:
-            mino[1] -= self.x
-            mino[0] -= self.y
-            tmp = mino[0]
-            mino[0] = mino[1] + self.y
-            mino[1] = -tmp + self.x
+        self.fill(new, (0, 0, 0))
+        tmp = self.width
+        self.width = self.height
+        self.height = tmp
+        for pos in self.location:
+            pos[1] -= self.center[1]
+            pos[0] -= self.center[0]
+            tmp = pos[1]
+            pos[1] = int(pos[0] + self.center[1])
+            pos[0] = int((-1 * tmp) + self.center[0])
         if self.collide():
-            for mino in self.location:
-                mino[1] -= self.x
-                mino[0] -= self.y
-                tmp = mino[0]
-                mino[0] = -mino[1] + self.y
-                mino[1] = tmp + self.x
+            for pos in self.location:
+                pos[1] -= self.center[1]
+                pos[0] -= self.center[0]
+                tmp = pos[1]
+                pos[1] = int((-1 * pos[0]) + self.center[1])
+                pos[0] = int(tmp + self.center[0])
+                tmp = self.width
+                self.width = self.height
+                self.height = tmp
+
         else:
-            minimum_y = self.location[0][0]
-            min_cols = [self.location[0][1]]
-            for i in range(1, 4):
-                if self.location[i][0] < minimum_y:
-                    minimum_y = self.location[i][0]
-                    min_cols = [self.location[i][1]]
-                if self.location[i][0] == minimum_y:
-                    min_cols.append(self.location[i][1])
-
-            minimum_x = min(min_cols)
-
-            self.x = minimum_x
-            self.y = minimum_y
-            tmp = self.width
-            self.width = self.height
-            self.height = tmp
-            self.fill(new)
+            self.reset_center()
+            self.x = round(self.center[1] - self.width/2)
+            self.y = round(self.center[0] - self.height/2)
+        self.fill(new, self.color)
 
     def move(self, x, y):
-        self.fill(new, 0)
-        self.x += x
-        self.y += y
-        for i in self.location:
-            i[0] += y
-            i[1] += x
+        if max(self.location)[0] + y < 24:
+            self.fill(new, (0, 0, 0))
+            self.x += x
+            self.y += y
+            self.center[0] += y
+            self.center[1] += x
 
-        if self.collide():
-            self.x -= x
-            self.y -= y
             for i in self.location:
-                i[0] -= y
-                i[1] -= x
-        self.fill(new)
+                i[0] += y
+                i[1] += x
+            if self.collide():
+                self.x -= x
+                self.y -= y
+                self.center[0] -= y
+                self.center[1] -= x
+                for i in self.location:
+                    i[0] -= y
+                    i[1] -= x
+            self.fill(new, self.color)
 
 
 minos = ['I', 'L', 'J', 'S', 'Z', 'T', 'O']
@@ -141,23 +166,29 @@ bg = pygame.image.load('Assets/bg.png')
 
 if __name__ == '__main__':
     done = False
+    count = 0
+    rot = False
     while not done:
-        clock.tick(15)
+        clock.tick(10)
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 done = True
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] and block.x + block.height < len(new.grid[0]):
-            block.rotate()
-            if block.x - block.width < 0:
-                block.move(abs(block.x-block.width), 0)
-        if keys[pygame.K_LEFT] and block.x != 0:
-            block.move(-1, 0)
-        if keys[pygame.K_RIGHT] and block.x + block.width < len(new.grid[0]):
+        if keys[pygame.K_UP] and (block.center[0] + block.width/2 < 24 and block.center[1] + block.height/2 < 10):
+            if block.center[1] - block.height/2 >= 0:
+                block.rotate()
+                rot = True
+        elif keys[pygame.K_RIGHT] and (block.center[1] + block.width/2 <= 9):
             block.move(1, 0)
+            count = 0
+        elif keys[pygame.K_LEFT] and block.x - 1 >= 0:
+            block.move(-1, 0)
+            count = 0
         if keys[pygame.K_ESCAPE]:
             break
-        screen.fill(0)
+        screen.fill((0, 0, 0))
+        screen.blit(bg, (0, 0))
+
         new.draw(screen)
         if block.y + block.height < 24:
             prev = block.y
@@ -165,8 +196,20 @@ if __name__ == '__main__':
             if block.y == 0:
                 break
             if prev == block.y:
-                new.check_clear()
-                block = Tetromino(0, 0, random.choice(minos))
+                count += 1
+                if count >= 2:
+                    new.check_clear()
+                    block = Tetromino(0, 0, random.choice(minos))
+        elif block.y + block.height == 24:
+            prev = block.y
+            if block.y == 0:
+                break
+            if prev == block.y:
+                count += 1
+                if count >= 2:
+                    new.check_clear()
+                    block = Tetromino(0, 0, random.choice(minos))
+
         else:
             new.check_clear()
             block = Tetromino(0, 0, random.choice(minos))
